0328f652615183d9f95e9dd9b8758ba9e9c356a1
Patrick Smith <pgwsmith@gmail.com>
2016-11-08 22:51:54 +1100

    Require env TRELLO_BOARD_ID in start.js

--- start.js
+++ start.js
@@ -4,4 +4,8 @@ const startServerForBoard = require('./src/startServerForBoard')
 
 const boardID = process.env.TRELLO_BOARD_ID
 
+if (!boardID || boardID.length === 0) {
+    throw "No board ID specified: env var TRELLO_BOARD_ID"
+}
+
 startServerForBoard(boardID)
