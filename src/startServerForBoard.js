9dbce4d788cdf9d30d3a5444485f7de958b3fb79
Patrick Smith <pgwsmith@gmail.com>
2016-11-22 19:20:13 +1100

    Add reloading of Trello board

--- src/startServerForBoard.js
+++ src/startServerForBoard.js
@@ -11,55 +11,97 @@ function startServerForBoard(boardID, { seo = true } = {}) {
         port: (process.env.PORT || 80)
     })
 
-    Axios.get(`https://trello.com/b/${ boardID }.json`)
-    .then(response => {
-        console.log('Loaded from Trello', boardID)
-        const boardJSON = response.data
+    server.route({
+        method: 'GET',
+        path: '/-raw',
+        handler(request, reply) {
+            reply(JSON.stringify(boardJSON, null, 2))
+        }
+    })
 
+    if (seo) {
         server.route({
             method: 'GET',
-            path: '/-raw',
+            path: '/robots.txt',
             handler(request, reply) {
-                reply(JSON.stringify(boardJSON, null, 2))
-            }
-        })
-
-        if (seo) {
-            server.route({
-                method: 'GET',
-                path: '/robots.txt',
-                handler(request, reply) {
-                    reply(
+                reply(
 `User-agent: *
 Disallow:
 `
 
 // Sitemap: https://burntcaramel.github.io/sitemap.xml
-                    ).type('text/plain')
-                }
-            })
-
-            // TODO: sitemap
-        }
-
-        promiseEnhancedTrelloCards(boardJSON.cards)
-        .then((cards) => {
-            console.log('Enhanced cards')
-
-            const enhancedBoardJSON = R.merge(boardJSON, { cards })
-            server.route(routesForTrelloData(enhancedBoardJSON))
-            
-            server.start()
-
-            // Prevent exit
-            process.stdin.resume()
-
-            console.log('Started server')
+                ).type('text/plain')
+            }
         })
+
+        // TODO: sitemap
+    }
+
+    let reloadableServer
+
+    function reloadFromTrello() {
+        return Axios.get(`https://trello.com/b/${ boardID }.json`)
+        .then(({ data: boardJSON }) => {
+            console.log('Loaded from Trello', boardID)
+
+            return promiseEnhancedTrelloCards(boardJSON.cards)
+            .then((cards) => {
+                console.log('Enhanced cards')
+
+                const enhancedBoardJSON = R.merge(boardJSON, { cards })
+
+                reloadableServer = new Hapi.Server()
+                const reloadableConnection = reloadableServer.connection({ autoListen: false })
+                reloadableServer.route(routesForTrelloData(enhancedBoardJSON))
+
+                return { success: true }
+            })
+        })
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/-reload/{token}',
+        handler(request, reply) {
+            reloadFromTrello()
+            .then(
+                reply,
+                reply
+            )
+        }
     })
-    .catch(error => {
-        console.error(error)
+
+    server.route({
+        method: '*',
+        path: '/{p*}',
+        handler(request, reply) {
+            console.log(request.url, request.params)
+            reloadableServer.inject(request.url.href)
+            .then((innerResponse) => {
+                console.log(innerResponse)
+                const outerResponse = reply(innerResponse.rawPayload)
+                outerResponse.code(innerResponse.statusCode)
+                Object.keys(innerResponse.headers).map((key) => {
+                    outerResponse.header(key, innerResponse.headers[key])
+                })
+            })
+        }
     })
+
+    reloadFromTrello()
+    .then(
+        () => {
+            server.start()
+        },
+        (error) => {
+            console.error('Error loading from Trello', error)
+        }
+    )
+    
+    // Prevent exit
+    process.stdin.resume()
+
+    console.log('Started server')
 }
 
 module.exports = startServerForBoard
